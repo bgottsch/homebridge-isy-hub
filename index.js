8ef8f1ec6431cb61d9aa41fd49c9aaa4faf0e49c
bgottsch <beno@me.com>
2020-04-11 23:52:17 -0300

    fix

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const ISYPlatformModule = require('./lib/platform');
 const ISYPlatform = ISYPlatformModule.ISYPlatform;
 
 module.exports = function (homebridge) {
-	ISY994PlatformModule.setHomebridge(homebridge);
+	ISYPlatformModule.setHomebridge(homebridge);
 	homebridge.registerPlatform('homebridge-isy', 'ISY-994i', ISYPlatform, dynamic);
 };
